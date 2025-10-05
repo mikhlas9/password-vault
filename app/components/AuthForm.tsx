99fe392763952aac349ca1eeedaa0c958e171aea
mikhlas9 <mohammadikhlas99@gmail.com>
2025-10-05 10:08:48 +0530

    &apos; fixed

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -93,7 +93,7 @@ export default function AuthForm({ type }: AuthFormProps) {
             <p className="text-center text-sm text-gray-600">
               {isLogin ? (
                 <>
-                  Don't have an account?{' '}
+                  Don&apos;t have an account?{' '}
                   <Link href="/register" className="text-blue-600 hover:text-blue-500 font-medium transition-colors">
                     Sign up
                   </Link>
